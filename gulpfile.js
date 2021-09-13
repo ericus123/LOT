16b6943aec8be8f6ad50599105ab7608593b3d6e
AMANI Eric <amaniericus@gmail.com>
2021-09-13 19:35:44 +0200

    setup sass

---
+++ gulpfile.js
@@ -0,0 +1,14 @@
+const { src, dest, watch, series } = require("gulp");
+const sass = require("gulp-sass")(require("sass"));
+
+buildStyles = () => {
+  return src("./styles/sass/index.scss")
+    .pipe(sass())
+    .pipe(dest(`./styles/css`));
+};
+
+const watchChange = () => {
+  watch(["styles/sass/index.scss"], buildStyles);
+};
+
+exports.default = series(buildStyles, watchChange);
